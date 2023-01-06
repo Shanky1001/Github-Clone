b20e24dc1d6c14b18c3c90e1cd40a194be9c7cf3
Shashank Rai <108118506+shanky-ced@users.noreply.github.com>
2023-01-06 11:16:40 +0530

    Update DetailsPage.jsx

--- src/Components/DetailsPage.jsx
+++ src/Components/DetailsPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from './Navbar'
-import '../Styles/DetailsPage.css'
+import React from 'react';
+import Navbar from './Navbar';
+import '../Styles/DetailsPage.css';
 import TabArea from './Tabs';
 
 
